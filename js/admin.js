aa967c7719e0f04f4e096e660be4ee7173266915
John Daniel Castro <jodcastrori@unal.edu.co>
2015-11-20 18:00:07 -0500

    Cambios a los eventos de Crear Asignatura

--- js/admin.js
+++ js/admin.js
@@ -7,17 +7,11 @@ $(document).ready(function () {
 
     $('.boton-editar-asig').leanModal({
         dismissible: false
-    }); //Activar modales para Crear Asignatura
+    }); //Activar modales para Editar Asignatura
 
     $('.boton-eliminar-asig').leanModal({
         dismissible: false
     }); //Activar modales para Eliminar Asignatura
-
-    $permiso = $('#permiso-login').val();
-
-    if ($permiso === "true") {
-        Materialize.toast("No tiene permisos suficientes para acceder a esa página", 60000, "rounded toast_error");
-    }
 
     $('.boton-crear-prof').leanModal({
         dismissible: false
@@ -25,11 +19,19 @@ $(document).ready(function () {
 
     $('.boton-editar-prof').leanModal({
         dismissible: false
-    }); //Activar modales para Crear Profesor
+    }); //Activar modales para Editar Profesor
 
     $('.boton-eliminar-prof').leanModal({
         dismissible: false
     }); //Activar modales para Eliminar Profesor
+
+    $permiso = $('#permiso-login').val();
+
+    if ($permiso === "true") {
+        Materialize.toast("No tiene permisos suficientes para acceder a esa página", 60000, "rounded toast_error");
+    }
+
+    //-----------------Uso de cookies para renderizar mensajes Toast luego de una redirección----------------------
 
     if (Cookies.get("crear_profesor") === "true") {
         Cookies.remove("crear_profesor");
@@ -66,9 +68,12 @@ $(document).ready(function () {
         Cookies.remove("eliminar_asignatura");
         Materialize.toast("La asignatura a eliminar no existe", 60000, "rounded toast_error");
     };
+
+    //-------------------------------------------------------------------------------------------------------------
+
 });
 
-function validar_campo_vacio (campo) {
+function validar_campo (campo) {
     $id_campo = campo.attr('id');
     $label = campo.parent().parent().parent().find('label[for="' + $id_campo + '"]');
 
@@ -76,18 +81,7 @@ function validar_campo_vacio (campo) {
         campo.addClass('invalid');
         $label.addClass('active');
         $label.attr('data-error', "Campo Obligatorio");
-    } else {
-        if (campo.hasClass('invalid')) {
-            campo.removeClass('invalid');
-        }
-    }
-}
-
-function validar_campo_numerico (campo) {
-    $id_campo = campo.attr('id');
-    $label = campo.parent().parent().parent().find('label[for="' + $id_campo + '"]');
-
-    if (!$.isNumeric(campo.val())) {
+    } else if (!$.isNumeric(campo.val())){ 
         campo.addClass('invalid');
         $label.addClass('active');
         $label.attr('data-error', "Campo Numérico");
@@ -160,15 +154,15 @@ $('#conf-login').on('click',function () {
 });
 
 
-//---------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------
 
-//--------------------------------Crear Asignatura-------------------------------
+//-----------------------------------------------Crear Asignatura---------------------------------------------
 
 $('.input-crear-asig').on('blur change keyup', function () {
-    validar_campo_vacio($(this));
+    validar_campo($(this));
 });
 
-$('#div-principal-crear-asig #id, #div-principal-crear-asig #creditos').on('blur change paste keyup', function () {
+$('#div-principal-crear-asig #id, #div-principal-crear-asig #creditos').on('blur', function () {
     $form = $('#div-principal-crear-asig').find('form');
 
     $form.submit(function (event) {
@@ -235,28 +229,28 @@ $('#conf-guardar-asig, #conf-crear-grupo-asig').on('click', function () {
                 }
 
                 if (!$mensaje.id3) {
-                    validar_campo_numerico($('#div-principal-crear-asig #id'));
+                    validar_campo($('#div-principal-crear-asig #id'));
 
                     Materialize.toast('El código debe ser numérico', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.creditos2) {
-                    validar_campo_numerico($('#div-principal-crear-asig #creditos'));
+                    validar_campo($('#div-principal-crear-asig #creditos'));
 
                     Materialize.toast('Los créditos deben ser numéricos', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.id2 || !$mensaje.nombre || !$mensaje.escuela || !$mensaje.creditos1) {
                     if (!$mensaje.id2) {
-                        validar_campo_vacio($('#div-principal-crear-asig #id'));
+                        validar_campo($('#div-principal-crear-asig #id'));
                     }
 
                     if (!$mensaje.creditos1) {
-                        validar_campo_vacio($('#div-principal-crear-asig #creditos'));
+                        validar_campo($('#div-principal-crear-asig #creditos'));
                     } 
 
-                    validar_campo_vacio($('#div-principal-crear-asig #nombre'));
-                    validar_campo_vacio($('#div-principal-crear-asig #escuela'));
+                    validar_campo($('#div-principal-crear-asig #nombre'));
+                    validar_campo($('#div-principal-crear-asig #escuela'));
 
                     Materialize.toast('Hay campos obligatorios sin diligenciar', 10000, 'rounded toast_error');
                 }
@@ -268,7 +262,6 @@ $('#conf-guardar-asig, #conf-crear-grupo-asig').on('click', function () {
                     } else if ($boton_id === "conf-crear-grupo-asig"){
                         mostrar_crear_grupo("crear");
                     }
-                    
                 }
             }    
         });
@@ -286,7 +279,7 @@ $('#conf-guardar-asig, #conf-crear-grupo-asig').on('click', function () {
 //--------------------------------Editar Asignatura-------------------------------
 
 $('.input-editar-asig').on('blur change keyup', function () {
-    validar_campo_vacio($(this));
+    validar_campo($(this));
 });
 
 $('#div-principal-editar-asig #id, #div-principal-editar-asig #creditos').on('blur change paste keyup', function () {
@@ -356,28 +349,28 @@ $('#conf-editar-asig').on('click', function () {
                 }
 
                 if (!$mensaje.id3) {
-                    validar_campo_numerico($('#div-principal-editar-asig #id'));
+                    validar_campo($('#div-principal-editar-asig #id'));
 
                     Materialize.toast('El código debe ser numérico', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.creditos2) {
-                    validar_campo_numerico($('#div-principal-editar-asig #creditos'));
+                    validar_campo($('#div-principal-editar-asig #creditos'));
 
                     Materialize.toast('Los créditos deben ser numéricos', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.id2 || !$mensaje.nombre || !$mensaje.escuela || !$mensaje.creditos1) {
                     if (!$mensaje.id2) {
-                        validar_campo_vacio($('#div-principal-editar-asig #id'));
+                        validar_campo($('#div-principal-editar-asig #id'));
                     }
 
                     if (!$mensaje.creditos1) {
-                        validar_campo_vacio($('#div-principal-editar-asig #creditos'));
+                        validar_campo($('#div-principal-editar-asig #creditos'));
                     } 
 
-                    validar_campo_vacio($('#div-principal-editar-asig #nombre'));
-                    validar_campo_vacio($('#div-principal-editar-asig #escuela'));
+                    validar_campo($('#div-principal-editar-asig #nombre'));
+                    validar_campo($('#div-principal-editar-asig #escuela'));
 
                     Materialize.toast('Hay campos obligatorios sin diligenciar', 10000, 'rounded toast_error');
                 }
@@ -574,7 +567,7 @@ $('.boton-eliminar-asig').on('click', function (e) {
 //-------------------------------Crear Profesor-------------------------------
 
 $('.input-crear-prof').on('blur change keyup', function () {
-    validar_campo_vacio($(this));
+    validar_campo($(this));
 });
 
 $('#div-principal-crear-prof #id').on('blur', function () {
@@ -634,18 +627,18 @@ $('#conf-crear-prof').on('click', function () {
                 }
 
                 if (!$mensaje.id3) {
-                    validar_campo_numerico($('#div-principal-crear-prof #id'));
+                    validar_campo($('#div-principal-crear-prof #id'));
 
                     Materialize.toast('La cédula debe ser numérica', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.id2 || !$mensaje.nombre || !$mensaje.escuela) {
                     if (!$mensaje.id2) {
-                        validar_campo_vacio($('#div-principal-crear-prof #id'));
+                        validar_campo($('#div-principal-crear-prof #id'));
                     } 
 
-                    validar_campo_vacio($('#div-principal-crear-prof #nombre'));
-                    validar_campo_vacio($('#div-principal-crear-prof #escuela'));
+                    validar_campo($('#div-principal-crear-prof #nombre'));
+                    validar_campo($('#div-principal-crear-prof #escuela'));
 
                     Materialize.toast('Hay campos obligatorios sin diligenciar', 10000, 'rounded toast_error');
                 }
@@ -670,7 +663,7 @@ $('#conf-crear-prof').on('click', function () {
 //-----------------------------Editar Profesor-----------------------------------
 
 $('.input-editar-prof').on('blur change keyup', function () {
-    validar_campo_vacio($(this));
+    validar_campo($(this));
 });
 
 $('#div-principal-editar-prof #id').on('blur change paste keyup', function () {
@@ -741,18 +734,18 @@ $('#conf-prof-edit').on('click', function () {
                 }
 
                 if (!$mensaje.id3) {
-                    validar_campo_numerico($('#div-principal-editar-prof #id'));
+                    validar_campo($('#div-principal-editar-prof #id'));
 
                     Materialize.toast('La cédula debe ser numérica', 10000, 'rounded toast_error');
                 }
 
                 if (!$mensaje.id2 || !$mensaje.nombre || !$mensaje.escuela) {
                     if (!$mensaje.id2) {
-                        validar_campo_vacio($('#div-principal-editar-prof #id'));
+                        validar_campo($('#div-principal-editar-prof #id'));
                     } 
 
-                    validar_campo_vacio($('#div-principal-editar-prof #nombre'));
-                    validar_campo_vacio($('#div-principal-editar-prof #escuela'));
+                    validar_campo($('#div-principal-editar-prof #nombre'));
+                    validar_campo($('#div-principal-editar-prof #escuela'));
 
                     Materialize.toast('Hay campos obligatorios sin diligenciar', 10000, 'rounded toast_error');
                 }
